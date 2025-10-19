7e55a1a472c23888420ff9e7c0fc6817f7b03b9f
S6U5 <shin2001u@gmail.com>
2025-10-19 11:15:09 +0900

    output:exportの追加

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+    images: { unoptimized: true }, // next/image を使うなら推奨
 };
 
 export default nextConfig;
