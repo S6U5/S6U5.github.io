d904b8c3105f53d055a603f422ad64010177d5c8
S6U5 <168543374+S6U5@users.noreply.github.com>
2025-11-07 23:32:26 +0900

    Mainページ作成前

--- components/main-contents/MainContent.tsx
+++ components/main-contents/MainContent.tsx
@@ -2,47 +2,51 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="flex flex-wrap gap-4">
-      <div className="relative w-[300px] aspect-square">
-        <Image
-          src="/images/JK_Adventure_title.png"
-          alt="A"
-          fill
-          className="object-cover rounded-xl"
-          sizes="300px"
-          priority
-        />
-      </div>
-      <div className="relative w-[300px] aspect-square">
-        <Image
-          src="/images/JK_Adventure_title.png"
-          alt="B"
-          fill
-          className="object-cover rounded-xl"
-          sizes="300px"
-          priority
-        />
-      </div>
-      <div className="relative w-[300px] aspect-square">
-        <Image
-          src="/images/JK_Adventure_title.png"
-          alt="B"
-          fill
-          className="object-cover rounded-xl"
-          sizes="300px"
-          priority
-        />
-      </div>
-      <div className="relative w-[300px] aspect-square">
-        <Image
-          src="/images/JK_Adventure_title.png"
-          alt="B"
-          fill
-          className="object-cover rounded-xl"
-          sizes="300px"
-          priority
-        />
-      </div>
-    </div>
+    <>
+      経歴やportfolioを見やすく整理したページです！
+      いったんデプロイします！(2025/11/07)
+    </>
+    // <div className="flex flex-wrap gap-4">
+    //   <div className="relative w-[300px] aspect-square">
+    //     <Image
+    //       src="/images/JK_Adventure_title.png"
+    //       alt="A"
+    //       fill
+    //       className="object-cover rounded-xl"
+    //       sizes="300px"
+    //       priority
+    //     />
+    //   </div>
+    //   <div className="relative w-[300px] aspect-square">
+    //     <Image
+    //       src="/images/JK_Adventure_title.png"
+    //       alt="B"
+    //       fill
+    //       className="object-cover rounded-xl"
+    //       sizes="300px"
+    //       priority
+    //     />
+    //   </div>
+    //   <div className="relative w-[300px] aspect-square">
+    //     <Image
+    //       src="/images/JK_Adventure_title.png"
+    //       alt="B"
+    //       fill
+    //       className="object-cover rounded-xl"
+    //       sizes="300px"
+    //       priority
+    //     />
+    //   </div>
+    //   <div className="relative w-[300px] aspect-square">
+    //     <Image
+    //       src="/images/JK_Adventure_title.png"
+    //       alt="B"
+    //       fill
+    //       className="object-cover rounded-xl"
+    //       sizes="300px"
+    //       priority
+    //     />
+    //   </div>
+    // </div>
   );
 }
